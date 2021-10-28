eff3a9afa3872331eb7e0fcd97aa92e4962f8725
Victor <felipe.beleza29@gmail.com>
2021-10-27 20:46:20 -0400

    [Swagger] Atualizações nas configurações swagger

--- src/main/java/com/sessionmanager/config/SwaggerConfig.java
+++ src/main/java/com/sessionmanager/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.sessionmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,12 +14,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private static  final String TITLE="VOTE API";
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.sessionmanager.controller"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(TITLE)
                 .build();
     }
 }
--- src/test/java/com/sessionmanager/service/impl/PautaManagerServiceImplTest.java
+++ src/test/java/com/sessionmanager/service/impl/PautaManagerServiceImplTest.java
@@ -2,8 +2,6 @@ package com.sessionmanager.service.impl;
 
 import com.sessionmanager.model.Pauta;
 import com.sessionmanager.repository.PautaRepository;
-import javassist.NotFoundException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,14 +12,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.Date;
-import java.util.Optional;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
